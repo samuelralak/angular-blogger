3e2d97ccbc79cee26c3aa7f02760d517319f8b8e
samuelralak <samuelralak@ovi.com>
2015-04-06 14:17:30 +0300

    implemented the permalink filter

--- app/modules/admin/js/filters.js
+++ app/modules/admin/js/filters.js
@@ -1,3 +1,11 @@
 'use strict';
 
 angular.module('mainApp.admin.filters', []);
+
+function permalink () {
+	return function (title) {
+		return title===undefined ? '' : angular.lowercase(title).replace(/[\s]/g,'-');
+	}
+}
+
+angular.module('mainApp.admin.filters'	).filter('permalink', permalink);
