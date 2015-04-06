b9183ff1be40f175adaeb872161278545e7789c1
samuelralak <samuelralak@ovi.com>
2015-04-07 02:27:08 +0300

    commentsDirective fix

--- app/modules/posts/js/directives.js
+++ app/modules/posts/js/directives.js
@@ -7,20 +7,20 @@ function commentsDirective (Post) {
 		restrict: 'AEC',
 		scope: { postInstance: '=' },
 		replace: true,
-		link:['scope', 'elem', 'attrs', function (scope, elem, attrs) {
+		link:function (scope, elem, attrs) {
 			scope.saveComment = function() {
 				var postID = scope.postInstance._id, 
 					savedPostInstance = {};
 				scope.comment.datePublished = new Date();
-				angular.copy(scope.postInstance, savedPostInstance); //copy the post instance in `scope` to a variable `savedPostInstance`
-				savedPostInstance.comments.unshift(scope.comment); 	//push the comment to the savedPostInstance
-				scope.postInstance.comments.unshift(scope.comment); //push the comment to the `scope.postInstance` as well
-				scope.comment = {}; // clear the comment
-				savedPostInstance.$update(); //Now update `savedPostInstance` so that the new comment goes to the server.
+				angular.copy(scope.postInstance, savedPostInstance); 
+				savedPostInstance.comments.unshift(scope.comment); 	
+				scope.postInstance.comments.unshift(scope.comment); 
+				scope.comment = {}; 
+				savedPostInstance.$update(); 
 			}
-		}],
+		},
 		templateUrl:'modules/posts/views/comments.html'
 	}
 }
 
-angular.module('mainApp.posts.directives').directive('commentsDirective', commentsDirective);
+angular.module('mainApp.posts.directives').directive('commentsDirective', ['Post', commentsDirective]);
