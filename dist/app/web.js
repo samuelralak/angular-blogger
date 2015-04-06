cedd794c7835c3783d66679d38065bab5da2006c
samuelralak <samuelralak@ovi.com>
2015-04-06 19:56:21 +0300

    logger error fix first attempt

--- dist/app/web.js
+++ dist/app/web.js
@@ -1,8 +1,7 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
-var logger = require('morgan');
 
-app.use(express.logger('dev'));
+app.use(express.morgan('dev'));
 app.use(gzippo.staticGzip(__dirname + "/"));
 app.listen(process.env.PORT || 5000);
